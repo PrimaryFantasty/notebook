d2d271039d5e53ff2cfe281527267a3bda216ac4
PrimaryFantasty <36122477+PrimaryFantasty@users.noreply.github.com>
2019-12-28 22:53:20 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "          忘情錄           "
+site_name = "忘情錄"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-28T16:51+08:00"
 author = "Fantasty"
